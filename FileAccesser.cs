4bb69bdc0cff929134111b909aeb7797862a5c1f
Lev <levdjepko@gmail.com>
2020-03-13 10:44:31 -0700

    Minor updates

--- FileAccesser.cs
+++ FileAccesser.cs
@@ -9,16 +9,29 @@ namespace simpleExpenseTracking_withClasses
 {
     public static class FileAccesser
     {
-        public static void WriteToFile (double value)
+        public static void WriteToFile (object value)
         {
             //this will return a current project path, probably in bin / debug folder
             string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WriteLines2.txt");
             using (StreamWriter file =
              new StreamWriter(destPath, true))
             {
-                file.WriteLine(value);
+                file.WriteLine("Total is: " + String.Format("{0:0.##}", value));
                 file.WriteLine(DateTime.Now);
                 file.WriteLine("\n");
+            }
+            Console.WriteLine("These expenses were saved to a log file");
+        }
+
+        public static void WriteAValue(object value)
+        {
+            //this will return a current project path, probably in bin / debug folder
+            string destPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WriteLines2.txt");
+            using (StreamWriter file =
+             new StreamWriter(destPath, true))
+            {
+                file.Write(value);
+                file.Write(" ");
             }
         }
     }
